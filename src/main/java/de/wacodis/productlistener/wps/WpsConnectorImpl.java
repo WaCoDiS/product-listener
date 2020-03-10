9f191d0452492386e7cb39329d9fdf9234a96418
Sebastian Drost <s.drost@52north.org>
2020-03-10 18:43:02 +0100

    Fix WPS file downloading

--- src/main/java/de/wacodis/productlistener/wps/WpsConnectorImpl.java
+++ src/main/java/de/wacodis/productlistener/wps/WpsConnectorImpl.java
@@ -6,25 +6,7 @@
 package de.wacodis.productlistener.wps;
 
 import de.wacodis.productlistener.WpsMetadataExecption;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.logging.Level;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
+import org.apache.commons.io.FileUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -41,25 +23,34 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 /**
- *
  * @author matthes
  */
 @Component
 public class WpsConnectorImpl implements WpsConnector {
-    
+
     private static final Logger LOG = LoggerFactory.getLogger(WpsConnectorImpl.class);
-    
+
     private String wpsBaseUrl;
-    
+
     @Autowired
     private CloseableHttpClient httpClient;
-    
+
     private WPSClientSession wpsSession;
-    
+
     private String storageDirectory;
 
     @Value("${product-listener.wps-base-url}")
@@ -71,23 +62,23 @@ public class WpsConnectorImpl implements WpsConnector {
     public void setStorageDirectory(String storageDirectory) {
         this.storageDirectory = storageDirectory;
     }
-    
+
     @Autowired
     public void setWpsSession(WPSClientSession wpsSession) {
         this.wpsSession = wpsSession;
     }
-    
+
     @Bean
     public WPSClientSession wpsSession() {
         return new WPSClientSession();
     }
-    
+
     @Override
     public Map<String, Path> resolveProcessResult(String jobId, String... outputIdentifier) {
         try {
             Result result = this.wpsSession.retrieveProcessResult(this.wpsBaseUrl, jobId);
             Map<String, Path> resultingFiles = new HashMap<>();
-            
+
             for (String oi : outputIdentifier) {
                 int retriesOnOutput = 0;
                 boolean outputRetrieved = false;
@@ -102,25 +93,25 @@ public class WpsConnectorImpl implements WpsConnector {
                     }
                 }
             }
-            
+
             return resultingFiles;
         } catch (WpsMetadataExecption | IOException | URISyntaxException | WPSClientException ex) {
             LOG.warn("Could not process GetResult: " + ex.getMessage());
             LOG.debug(ex.getMessage(), ex);
         }
-        
+
         return Collections.emptyMap();
     }
 
     private Path storeFile(Data data) throws WpsMetadataExecption, URISyntaxException, IOException {
-        
+
         ComplexData complexData;
         if (data instanceof ComplexData) {
             complexData = ((ComplexData) data);
         } else {
             complexData = data.asComplexData();
         }
-        
+
         // we currently only support result by reference
         URI asUri;
         if (complexData.getReference() != null && complexData.getReference().getHref() != null) {
@@ -128,51 +119,40 @@ public class WpsConnectorImpl implements WpsConnector {
         } else {
             throw new WpsMetadataExecption("Could not resolve reference of process output: " + complexData);
         }
-        
+
         // two cases can happen here:
         // 1) no RequestBody: it is a plain HTTP GET link
         // 2) a RequestBody with Body or BodyReference: a HTTP POST must be done with the body
         // currently we only support HTTP GET
-        
+
         if (complexData.getReference().getBody() != null) {
             LOG.warn("A complex data with a POST body was found. This is currently not supported. POST body: " + complexData.getReference().getBody());
             throw new WpsMetadataExecption("Unsupported reference via HTTP POST");
         }
-        
+
         if (complexData.getReference().getBodyReference() != null) {
             LOG.warn("A complex data with a reference to POST body was found. This is currently not supported. POST body: " + complexData.getReference().getBodyReference());
             throw new WpsMetadataExecption("Unsupported reference via HTTP POST");
         }
-        
-        LOG.info("Downloading WPS result: {}", asUri);
-        
+
+        LOG.info("Start downloading WPS result: {}", asUri);
+
         String fileNamePrefix = DateTime.now().toString(DateTimeFormat.forPattern("yyyyMMdd_HHmm"));
         Path target = Paths.get(this.storageDirectory).resolve(fileNamePrefix + ".tif");
         HttpGet httpGet = new HttpGet(asUri);
-        
+
         // do the download
-        try (CloseableHttpResponse response = this.httpClient.execute(httpGet);
-                OutputStream fos = Files.newOutputStream(target);
-                InputStream is = response.getEntity().getContent();) {
-            
-            byte[] buf = new byte[1024];
-            int length;
-            long cycle = 0;
-            while( (length = is.read(buf)) != -1) {
-                 fos.write(buf);
-                 fos.flush();
-                 if (++cycle % (1000*1000*100) == 0) {
-                    LOG.debug("Downloaded {} mb", cycle * buf.length / (1000*1000));
-                }
-            }
-            
-            
+        try (CloseableHttpResponse response = this.httpClient.execute(httpGet);) {
+
+            InputStream is = response.getEntity().getContent();
+            FileUtils.copyInputStreamToFile(is, target.toFile());
+
         } catch (Exception e) {
             throw new IOException("Could not download result from WPS.", e);
         }
-        
+
         LOG.info("Result stored to: {}", target);
-        
+
         return target;
     }
 
@@ -180,11 +160,11 @@ public class WpsConnectorImpl implements WpsConnector {
     public String getProcessResult(String jobId, String outputIdentifier) throws IOException {
         try {
             Result result = this.wpsSession.retrieveProcessResult(this.wpsBaseUrl, jobId);
-            
+
             Optional<Data> o = result.getOutputs().stream().filter(d -> outputIdentifier.equalsIgnoreCase(d.getId())).findAny();
             if (o.isPresent()) {
                 Data data = o.get();
-                
+
                 if (data instanceof ComplexData) {
                     data.getValue();
                     return data.getValue().toString();
@@ -192,15 +172,15 @@ public class WpsConnectorImpl implements WpsConnector {
                     LiteralData literal = (LiteralData) data;
                     return literal.getValue().toString();
                 }
-                
+
             }
-            
+
         } catch (IOException | WPSClientException ex) {
             LOG.warn("Could not process GetResult: " + ex.getMessage());
             LOG.debug(ex.getMessage(), ex);
         }
-        
+
         throw new IOException("Could not resolve process job/output: " + jobId + "/" + outputIdentifier);
     }
-    
+
 }
