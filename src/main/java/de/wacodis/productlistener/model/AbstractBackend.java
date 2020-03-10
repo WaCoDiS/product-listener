a709bdd8993fa85289585344135f403098a2ce00
Sebastian Drost <s.drost@52north.org>
2020-03-10 08:09:10 +0100

    Update models

--- src/main/java/de/wacodis/productlistener/model/AbstractBackend.java
+++ src/main/java/de/wacodis/productlistener/model/AbstractBackend.java
@@ -16,11 +16,12 @@ import javax.validation.constraints.*;
  * abstract type for a backend that provides WaCoDiS products 
  */
 @ApiModel(description = "abstract type for a backend that provides WaCoDiS products ")
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-02-17T12:39:03.243+01:00[Europe/Berlin]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-03-10T08:01:49.445+01:00[Europe/Berlin]")
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "backendType", visible = true)
 @JsonSubTypes({
   @JsonSubTypes.Type(value = ArcGISImageServerBackend.class, name = "ArcGISImageServerBackend"),
+  @JsonSubTypes.Type(value = GeoServerBackend.class, name = "GeoServerBackend"),
 })
 
 public class AbstractBackend  implements Serializable {
